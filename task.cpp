c9f9e87782087a1ba82bf7ca0377e08924b3c1bd
Aydar <31863099+KhusainovAydar@users.noreply.github.com>
2018-01-20 22:16:20 +0300

    Update task.cpp

--- task.cpp
+++ task.cpp
@@ -1,35 +1,42 @@
 #include <iostream>
 
+template<typename T>
 struct Node {
-    int value;
+    T value;
+    bool isRoot;
     Node * next;
 
-    Node(int newValue)
+    Node(T newValue)
         : value(newValue)
         , next(nullptr)
+        , isRoot(false)
     {}
 
-    Node(){}
+    Node()
+        : isRoot(true)
+    {}
 };
 
+template<typename T>
 class List {
 public:
     List()
-        : first(nullptr)
-        , last(nullptr)
+        : first(new Node<T>())
+        , last(first)
         , num(0)
     {}
 
     ~List() {
         while (first != nullptr) {
-            Node * tmp = first;
+            Node<T> * tmp = first;
             first = first->next;
             delete tmp;
         }
     }
 
-    void set(Node * newFirst, Node * newLast, size_t newNum) {
-        first = newFirst;
+    void set(Node<T> * newFirst, Node<T> * newLast, size_t newNum) {
+        first = new Node<T>();
+        first->next = newFirst;
         last = newLast;
         num = newNum;
     }
@@ -38,11 +45,11 @@ public:
         set(nullptr, nullptr, 0);
     }
 
-    Node * get_first() const {
+    Node<T> * get_first() const {
         return first;
     }
 
-    Node * get_last() const {
+    Node<T> * get_last() const {
         return last;
     }
 
@@ -50,75 +57,89 @@ public:
         return num;
     }
 
-    void add(int value) {
-        ++num;
-        if (first == nullptr) {
-            first = new Node(value);
-            last = new Node(value);
+    void insertNode(Node<T> * node, T value) {
+        if (node == last) {
+            addValue(value);
         } else {
-            last->next = new Node(value);
-            last = last->next;
+            Node<T> * copy = new Node<T>(value);
+            copy->next = node->next;
+            node->next = copy;
+            ++num;
         }
     }
 
-    void add(Node * node) {
+    void addValue(T value) {
         ++num;
-        if (first == nullptr) {
-            first = last = node;
+        if (first == last) {
+            first->next = new Node<T>(value);
+            last = first->next;
         } else {
-            last->next = node;
+            last->next = new Node<T>(value);
             last = last->next;
         }
     }
 
 private:
-    Node * first;
-    Node * last;
+    Node<T> * first;
+    Node<T> * last;
     size_t num;
 };
 
-void merge_sort(List& list) {
+void merge(List<int>& left, List<int>& right) {
+    Node<int> * first = left.get_first();
+    Node<int> * second = right.get_first();
+    size_t numFirst = 0, numSecond = 0;
+    size_t leftSize = left.size(), rightSize = right.size();
+
+    while (numFirst < leftSize && numSecond < rightSize) {
+        int valFirst = first->next->value,
+                valSecond = second->next->value;
+
+        if (valFirst <= valSecond) {
+            first = first->next;
+            ++numFirst;
+        } else {
+            left.insertNode(first, valSecond);
+            first = first->next;
+            second = second->next;
+            ++numSecond;
+        }
+    }
+
+    while (numSecond < rightSize) {
+        int valSecond = second->next->value;
+        left.addValue(valSecond);
+        second = second->next;
+        ++numSecond;
+    }
+}
+
+void split(List<int>& list, size_t midSize, List<int>& left, List<int>& right) {
+    Node<int> * middle = list.get_first();
+    Node<int> * prevMiddle = new Node<int>();
+    middle = middle->next;
+
+    for (size_t i = 0; i != midSize; ++i) {
+        prevMiddle = middle;
+        middle = middle->next;
+    }
+    prevMiddle->next = nullptr;
+
+    left.set(list.get_first()->next, prevMiddle, midSize);
+    right.set(middle, list.get_last(), list.size() - midSize);
+}
+
+void merge_sort(List<int>& list) {
     if (list.size() > 1) {
         size_t midSize = list.size() / 2;
-        List left, right;
+        List<int> left, right;
 
-        Node * middle = list.get_first();
-        Node * prev_middle = new Node();
-        for (size_t i = 0; i != midSize; ++i) {
-            prev_middle = middle;
-            middle = middle->next;
-        }
-        prev_middle->next = nullptr;
-        left.set(list.get_first(), prev_middle, midSize);
-        right.set(middle, list.get_last(), list.size() - midSize);
-
+        split(list, midSize, left, right);
         merge_sort(left);
         merge_sort(right);
+        merge(left, right);
 
-        list.clear();
-        Node * first = left.get_first();
-        Node * second = right.get_first();
-
-        while (first != nullptr && second != nullptr) {
-            if (first->value <= second->value) {
-                list.add(first);
-                first = first->next;
-            } else {
-                list.add(second);
-                second = second->next;
-            }
-        }
-
-        while (first != nullptr)  {
-            list.add(first);
-            first = first->next;
-        }
-
-        while (second != nullptr)  {
-            list.add(second);
-            second = second->next;
-        }
-
+        list = left;
         left.clear();
         right.clear();
     }
@@ -128,17 +149,21 @@ int main() {
     size_t n;
     std::cin >> n;
 
-    List array;
+    List<int> array;
     for (size_t i = 0; i != n; ++i) {
         int x;
         std::cin >> x;
-        array.add(x);
+        array.addValue(x);
     }
 
     merge_sort(array);
 
-    Node * out = array.get_first();
+    Node<int> * out = array.get_first();
     while (out) {
+        if (out->isRoot) {
+            out = out->next;
+            continue;
+        }
         std::cout << out->value << ' ';
         out = out->next;
     }
